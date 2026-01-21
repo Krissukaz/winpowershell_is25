db60f90a34950a9cf1108453d508d7eac15b2a70
Kristofer Juursalu <kristofer.juursalu@voco.ee>
2026-01-21 09:44:31 +0200

    Kasuatja Loomine

---
+++ Lokaalne_Kasutaja_Loomine.ps1
@@ -0,0 +1,18 @@
+﻿$Eesnimi = Read-Host "Sisesta oma eesnimi"
+$Perenimi = Read-Host "Sisesta oma perenimi"
+
+$KasutajaNimi = "$Eesnimi.$Perenimi".ToLower()
+$Taisnimi = "$Eesnimi $Perenimi"
+
+Write-Host "Loodav kasutaja on $KasutajaNimi"
+
+$ParoolTekst = "Parool1!"
+$KasutajaParool = ConvertTo-SecureString $ParoolTekst -AsPlainText -Force
+
+New-LocalUser -Name $KasutajaNimi -Password $KasutajaParool -FullName $Taisnimi -Description "Loodud skriptiga" -ErrorAction SilentlyContinue
+
+if ($?) {
+    Write-Host "Kasutaja $KasutajaNimi loomine õnnestus!" -ForegroundColor Green
+} else {
+    Write-Host "Tekkinud probleem kasutaja loomisega" -ForegroundColor Red
+}
