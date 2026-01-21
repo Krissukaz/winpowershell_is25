87e962f15ec5baea819aabc3e500f3a9eeb638d4
Kristofer Juursalu <kristofer.juursalu@voco.ee>
2026-01-21 11:07:19 +0200

    Kasuatja Kustutamine

---
+++ Lokaalse_Kasutaja_Kustutamine.ps1
@@ -0,0 +1,14 @@
+﻿$firstName = Read-Host "Sisesta oma eesnimi"
+$lastName = Read-Host "Sisesta oma perenimi"
+
+$userName = "$($firstName).$($lastName)".ToLower()
+
+Write-Host "Kustutav kasutaja on $userName"
+
+try {
+    Remove-LocalUser -Name $userName -ErrorAction Stop
+    Write-Host "Kasutaja $userName on kustutatud"
+}
+catch {
+    Write-Host "Tekkinud probleem kasutaja kustutamisega"
+}
